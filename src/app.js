cbcdd7f38e681eec9894a9ea75b023c3e6ffd9c5
Discounnt Studios <123641634+JustNullus@users.noreply.github.com>
2023-06-21 19:56:28 -0400

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,157 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+function speak(sentence) {
+    const text_speak = new SpeechSynthesisUtterance(sentence);
+
+    text_speak.rate = .9;
+    text_speak.pitch = .9;
+
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe() {
+    var day = new Date();
+    var hr = day.getHours();
+
+    if(hr >= 0 && hr < 12) {
+        speak("Good Morning Boss");
+    }
+
+    else if(hr == 12) {
+        speak("Good noon Boss");
+    }
+
+    else if(hr > 12 && hr <= 17) {
+        speak("Good Afternoon Boss");
+    }
+
+    else {
+        speak("Good Evening Boss");
+    }
+}
+
+window.addEventListener('load', ()=>{
+    speak("Activating Eli");
+    wishMe();
+})
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event) => {
+    const current = event.resultIndex;
+    const transcript = event.results[current][0].transcript;
+    content.textContent = transcript;
+    speakThis(transcript.toLowerCase());
+}
+
+btn.addEventListener('click', ()=>{
+    recognition.start();
+})
+
+function speakThis(message) {
+    const speech = new SpeechSynthesisUtterance();
+
+    speech.text = "I did not understand what you said please try again";
+
+    if(message.includes('hey') || message.includes('hello')) {
+        const finalText = "Hello Boss";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('how are you')) {
+        const finalText = "I am fine boss tell me how can i help you";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('your name')) {
+        const finalText = "My name is Eli";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('open google')) {
+        window.open("https://google.com", "_blank");
+        const finalText = "Opening Google";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('open instagram')) {
+        window.open("https://instagram.com", "_blank");
+        const finalText = "Opening instagram";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('Drop the beat')) {
+        window.open("https://www.youtube.com/watch?v=ZlJnJ2VQT9s");
+        const finalText = "Dropping the beat LOL";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('play my mix')) {
+        window.open("https://open.spotify.com/collection/tracks");
+        const finalText = "lets get down";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('what is') || message.includes('who is') || message.includes('what are')) {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "This is what i found on internet regarding " + message;
+        speech.text = finalText;
+    }
+
+    else if(message.includes('wikipedia')) {
+        window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "")}`, "_blank");
+        const finalText = "This is what i found on wikipedia regarding " + message;
+        speech.text = finalText;
+    }
+
+    else if(message.includes('time')) {
+        const time = new Date().toLocaleString(undefined, {hour: "numeric", minute: "numeric"})
+        const finalText = time;
+        speech.text = finalText;
+    }
+
+    else if(message.includes('date')) {
+        const date = new Date().toLocaleString(undefined, {month: "short", day: "numeric"})
+        const finalText = date;
+        speech.text = finalText;
+    }
+
+    else if(message.includes('calculator')) {
+        window.open('Calculator:///')
+        const finalText = "Opening Calculator";
+        speech.text = finalText;
+    }
+
+
+    else if(message.includes('Minecraft')) {
+        window.open('Minecraft:///')
+        const finalText = "Opening Minecraft";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('secedule')) {
+        window.open('calender:///')
+        const finalText = "Opening calender";
+        speech.text = finalText;
+    }
+
+    else if(message.includes('alexa')) {
+        const finalText = "alexa is my cousin's second brother's mother";
+        speech.text = finalText;
+    }
+
+
+    else {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "I found some information for " + message + " on google";
+        speech.text = finalText;
+    }
+
+    speech.volume = 3;
+    speech.pitch = 1;
+    speech.rate = .9;
+
+    window.speechSynthesis.speak(speech);
+}
